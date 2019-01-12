4a18ea14e07a8a04aff08e56f3220320a2615d75
kalpesh30 <kalpeshgupta30@gmail.com>
2019-01-12 14:38:18 +0530

    Resoklved the error in program

--- Week 1/roadNetwk.py
+++ Week 1/roadNetwk.py
@@ -4,25 +4,40 @@ Created on Thu Jan 10 11:59:48 2019
 
 @author: Kalpesh Gupta
 """
-import networkx as nx;
-import matplotlib as plt;
-import random;
+import networkx as nx
+import matplotlib.pyplot as plt
+"""import random """
+import numpy.random
 
 
 city = ['Delhi','Bangalore','Hyderabad','Surat','Ahemdabad','Chennai','Kolkata','Pune']
 
 G = nx.Graph();
 for eachCity in city:
-    G.add_node(eachCity);
+    G.add_node(eachCity)
 
-nx.draw(G,label);
-plt.show();
+nx.draw(G)
+plt.show()
+
 
 cost = [] 
 value = 100
 
 while(value < 2000):
-    costs.append(value)
-    value = value + 100;
+    cost.append(value)
+    value = value + 100
 
 print cost
+try:
+    while (G.number_of_edges() < 16):
+        c1 = numpy.random.choice(G.nodes())
+        c2 = numpy.random.choice(G.nodes())
+        if c1!=c2 and G.has_edge(c1,c2) == 0:
+            w = numpy.random.choice(cost)
+            G.add_edge(c1,c2,weight=w)
+except IndexError as error:
+    print "error"
+
+pos = nx.spectral_layout(G)
+nx.draw(G,pos)
+plt.show()
